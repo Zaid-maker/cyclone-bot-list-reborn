b02242895889be95b5d66c3e8f146b6fd15f5b3a
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-11-11 11:11:44 +0000

    format

--- src/structures/app.js
+++ src/structures/app.js
@@ -4,16 +4,18 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
 // Certificate
-const privateKey = fs.readFileSync('../../../../../etc/letsencrypt/live/faster-bots.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('../../../../../etc/letsencrypt/live/faster-bots.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('../../../../../etc/letsencrypt/live/faster-bots.com/chain.pem', 'utf8');
+//const privateKey = fs.readFileSync('../../../../../etc/letsencrypt/live/faster-bots.com/privkey.pem', 'utf8');
+//const certificate = fs.readFileSync('../../../../../etc/letsencrypt/live/faster-bots.com/cert.pem', 'utf8');
+//const ca = fs.readFileSync('../../../../../etc/letsencrypt/live/faster-bots.com/chain.pem', 'utf8');
 
-const { discord_client: {secret} } = require("@root/config.json");
+const {
+  discord_client: { secret },
+} = require("@root/config.json");
 
 require("@utils/passport.js");
 
@@ -40,36 +42,37 @@ class App {
     this.express.use(passport.initialize());
     this.express.use(passport.session());
     this.express.use((req, _, next) => {
-      if (!["/theme", "/login"].includes(req.originalUrl) && !req.originalUrl.startsWith("/api"))
-        req.session.url = req.originalUrl
-      next()
-    })
+      if (
+        !["/theme", "/login"].includes(req.originalUrl) &&
+        !req.originalUrl.startsWith("/api")
+      )
+        req.session.url = req.originalUrl;
+      next();
+    });
     this.loadRoutes().loadErrorHandler();
   }
 
   listen(port) {
-
-
-    const credentials = {
-      key: privateKey,
-      cert: certificate,
-      ca: ca
-    };
+    //const credentials = {
+    //  key: privateKey,
+    //  cert: certificate,
+    //  ca: ca,
+    //};
 
     this.express.use((req, res) => {
-      res.send('Hello there !');
+      res.send("Hello there !");
     });
 
     // Starting both http & https servers
     const httpServer = http.createServer(this.express);
-    const httpsServer = https.createServer(credentials, this.express);
+    const httpsServer = https.createServer(this.express);
 
-    httpServer.listen(80, () => {
-      console.log('HTTP Server running on port 80');
+    httpServer.listen(3000, () => {
+      console.log("HTTP Server running on port 80");
     });
 
-    httpsServer.listen(443, () => {
-      console.log('HTTPS Server running on port 443');
+    httpsServer.listen(3001, () => {
+      console.log("HTTPS Server running on port 443");
     });
     //return new Promise((resolve) => this.express.listen(port, resolve));
   }
@@ -99,7 +102,7 @@ class App {
   loadErrorHandler() {
     this.express.use((req, res) => {
       res.status(404);
-      if (req.accepts("html")) return res.render("404", {req});
+      if (req.accepts("html")) return res.render("404", { req });
 
       if (req.accepts("json"))
         return res.send({
--- src/utils/discordApi.js
+++ src/utils/discordApi.js
@@ -1,32 +1,43 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const { server: { role_ids: { bot_verifier } }, server: { admin_user_ids, id } } = require("@root/config.json")
+const {
+  server: {
+    role_ids: { bot_verifier },
+  },
+  server: { admin_user_ids, id },
+} = require("@root/config.json");
 
-module.exports.auth = async(req, res, next) => {
-    if (!req.user) return res.redirect("/login");
-    
-    req.user.staff = false;
+module.exports.auth = async (req, res, next) => {
+  if (!req.user) return res.redirect("/login");
 
-    try {
-        const member = await req.app.get('client').guilds.cache.get(id).members.fetch(req.user.id);
-        if (admin_user_ids.includes(req.user.id) || member.roles.cache.has(bot_verifier))
-            req.user.staff = true
-    } catch(_) {}
+  req.user.staff = false;
 
-    return next();
-}
+  try {
+    const member = await req.app
+      .get("client")
+      .guilds.cache.get(id)
+      .members.fetch(req.user.id);
+    if (
+      admin_user_ids.includes(req.user.id) ||
+      member.roles.cache.has(bot_verifier)
+    )
+      req.user.staff = true;
+  } catch (_) {}
+
+  return next();
+};
 
 module.exports.getUser = async (user) => {
-    let { accessToken } = user;
+  let { accessToken } = user;
 
-    user = await fetch(`https://discord.com/api/users/@me`, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
+  user = await fetch(`https://discord.com/api/users/@me`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
 
-    user = await user.json();
+  user = await user.json();
 
-    if (user.code === 0) return false;
-    return user;
+  if (user.code === 0) return false;
+  return user;
 };
